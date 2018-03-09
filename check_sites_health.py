a41e4a0a8df40bda55bb6d29088905d8054cd535
Daniil Shadrin <rabbit72rus@gmail.com>
2018-03-09 10:35:48 +0300

    add urls load from file

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,19 @@
-def load_urls4check(path):
-    pass
+
+
+def load_urls4check(path_to_urls):
+    with open(path_to_urls, 'r', encoding='utf8') as file_with_urls:
+        list_urls = file_with_urls.readlines()
+        return list(map(lambda x: x.rstrip(), list_urls))
+
 
 def is_server_respond_with_200(url):
     pass
 
+
 def get_domain_expiration_date(domain_name):
     pass
 
+
 if __name__ == '__main__':
-    pass
+    path_to_urls = 'urls'
+    urls4check = load_urls4check(path_to_urls)
